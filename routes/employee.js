c23016727bb069e643a5a0b7630cca9c200e61d3
Teran Sarathchandra <Teran.Sarathchandra@softlogiclife.lk>
2024-01-31 01:58:39 +0530

    Employee API Configuration and Project Structure Modified

--- routes/employee.js
+++ routes/employee.js
@@ -1,20 +1,58 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
 
-const Employee = require('../models/employee');
+const Employee = require('../models/employeeSchema');
+const EmployeeJoiSchema = require('../schemas/employeeSchema')
+
+router.get('/', async (req, res) => {     
+
+    const employee = await Employee.find();
+
+    if (!employee) {
+        return res.status(404).json({ status: 404, message: "Employee not found" });
+    }
+
+    res.status(200).json({ status: 200, data: employee, message: "Employees Found"});
+
+})
+
+router.get('/:id', async (req, res) => {
+
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ status: 404, error: "Invalid employee id"})
+    }
+
+    const employee = await Employee.findById(id);
+
+    if (!employee) {
+        return res.status(404).json({ status: 404, message: "Employee not found" });
+    }
+
+    res.status(200).json({ status: 200, data: employee, message: "Employee Found"});
+
+})
 
 router.post('/', async (req, res) => {
-    const { employeeId, name, contactNumber, password, accessLevelId, street, city, state } = req.body;
+
+    const { value, error } = EmployeeJoiSchema.validate(req.body);
+
+    if (error) {
+        return res.status(400).json({ status: 400, error: error });
+    }
+
+    const { employeeId, name, address, password, contactNumber, designationId, countryId } = value;
 
     const employee = await Employee.create({
         employeeId,
         name,
-        contactNumber,
+        address,
         password,
-        accessLevelId,
-        street,
-        city,
-        state
+        contactNumber,
+        designationId,
+        countryId
     });
 
     if (!employee) {
