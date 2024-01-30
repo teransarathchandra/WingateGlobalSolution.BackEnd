374cc7c77b3673d5a968b35c3d16cf279117c8b8
Prashan Samarathunge <prashansamarathunge@gmail.com>
2024-01-30 23:28:08 +0530

    "customerSchema.js"

---
+++ models/customerSchema.js
@@ -0,0 +1,22 @@
+
+const mongoose = require('mongoose');
+const { Schema, model} = mongoose;
+
+const customerSchema = new Schema({
+
+    //Need Foreign Keys. Remove this line after reviewing foreign keys.
+    priorityLevel:{
+        type : String,
+        enum : ['High Priority', 'Medium Priority', 'Low Priority'],
+        required :  true
+    },
+    customerId:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+        required :  true
+}
+},{ timestamps: true }
+);
+
+module.exports = model('customer', customerSchema);
+
