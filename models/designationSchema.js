21a134f08f4975cbd89cabd707d8300b2cc75535
Prashan Samarathunge <prashansamarathunge@gmail.com>
2024-01-30 23:28:07 +0530

    "designationSchema.js"

---
+++ models/designationSchema.js
@@ -0,0 +1,39 @@
+
+const mongoose = require('mongoose');
+const { Schema, model} = mongoose;
+
+const designationSchema = new Schema({
+
+    //Need Foreign Keys. Remove this line after reviewing foreign keys.
+    designationId:{
+            type : Number,
+            required :  true,
+            unique :  true
+    },
+    basicSalary:{
+            type : Float,
+            required :  true
+},
+    etf:{
+            type : Float,
+            required :  true
+},
+    epf:{
+            type : Float,
+            required :  true
+},
+    allowance:{
+            type : Float,
+            required :  true
+},
+    accessLevelId:{
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "systemAccess",
+                required :  true
+}
+
+},{ timestamps: true }
+);
+
+module.exports = model('designation', designationSchema);
+
