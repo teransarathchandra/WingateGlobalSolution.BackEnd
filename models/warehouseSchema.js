971ea165956b22615322d6cf06560d06431f8af0
Prashan Samarathunge <prashansamarathunge@gmail.com>
2024-01-30 23:28:07 +0530

    "warehouseSchema.js"

---
+++ models/warehouseSchema.js
@@ -0,0 +1,43 @@
+
+const mongoose = require('mongoose');
+const { Schema, model} = mongoose;
+
+const warehouseSchema = new Schema({
+
+    //Need Foreign Keys. Remove this line after reviewing foreign keys.
+    warehouseId:{
+            type : Number,
+            required :  true,
+            unique :  true
+    },
+    location:{
+            type : String,
+            required :  true,
+            maxLength :  255,
+            minLength :  10,
+            unique :  true
+    },
+    storageCapacity:{
+            type : Number,
+            required :  true
+},
+    availability:{
+            type : Boolean,
+            required :  true
+},
+    warehouseManagerId:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "employee",
+        required :  true
+},
+    countryId:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "country",
+        required :  true
+}
+
+},{ timestamps: true }
+);
+
+module.exports = model('warehouse', warehouseSchema);
+
