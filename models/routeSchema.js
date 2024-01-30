45eda92fe9136e1ea82d868d98aaacba7205d16a
Prashan Samarathunge <prashansamarathunge@gmail.com>
2024-01-30 23:28:07 +0530

    "routeSchema.js"

---
+++ models/routeSchema.js
@@ -0,0 +1,49 @@
+
+const mongoose = require('mongoose');
+const { Schema, model} = mongoose;
+
+const routeSchema = new Schema({
+
+        //Need Foreign Keys. Remove this line after reviewing foreign keys.
+        routeId:{
+                type : Number,
+                required :  true,
+                unique :  true
+        },
+        transportMode:{
+                type : String,
+                required :  true,
+                maxLength :  10,
+                unique :  true
+},
+        distance:{
+                type : Float,
+                required :  true
+},
+        priority:{
+                type : String,
+                required :  true
+},
+        roundTripDays:{
+                type : Number,
+                required :  true
+},
+        routeShippingCost:{
+                type : Float,
+                required :  true
+},
+        startingPortId:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "port",
+            required :  true
+},
+       endingPortId:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "port",
+            required :  true
+}
+    },{ timestamps: true }
+);
+
+module.exports = model('route', routeSchema);
+
