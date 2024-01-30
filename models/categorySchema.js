b14121174ed953494e73617f01613a8f9b2670d2
Prashan Samarathunge <prashansamarathunge@gmail.com>
2024-01-30 23:28:08 +0530

    "categorySchema.js"

---
+++ models/categorySchema.js
@@ -0,0 +1,36 @@
+
+const mongoose = require('mongoose');
+const { Schema, model} = mongoose;
+
+const categorySchema = new Schema({
+
+    //Need Foreign Keys. Remove this line after reviewing foreign keys.
+    categoryId:{
+            type : Number,
+            required :  true,
+            unique :  true
+    },
+    name:{
+            type : String,
+            maxLength : 50,
+            required :  true
+    },
+    description:{
+            type : String,
+            maxLength :  255
+    },
+    profitRate:{
+        type : Float,
+        required :  true,
+        min :  0
+    },
+    costPerKilo:{
+        type : Number,
+        required :  true,
+        min :  0
+    }
+},{ timestamps: true }
+);
+
+module.exports = model('category', categorySchema);
+
