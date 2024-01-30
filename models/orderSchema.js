d032a2e15fe25a20709674a98879ac9a33f4a086
Prashan Samarathunge <prashansamarathunge@gmail.com>
2024-01-30 23:28:07 +0530

    "orderSchema.js"

---
+++ models/orderSchema.js
@@ -0,0 +1,56 @@
+
+//Order
+const mongoose = require('mongoose');
+const { Schema, model} = mongoose;
+
+const orderSchema = new Schema({
+
+    //Need Foreign Keys. Remove this line after reviewing foreign keys.
+    orderId:{
+            type : Number,
+            required :  true,
+            unique :  true
+    },
+    
+    status:{
+        type : String,
+        required :  true,
+        maxLength :  255,
+        minLength :  10,
+        required :  true
+},
+    packageCount:{
+        type : Number,
+        default :  1
+    },
+
+    orderType:{
+        type : String,
+        required :  true
+},
+
+    userId:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+        required :  true
+},
+    routeId:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "route",
+        required :  true
+},
+   stockId:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "stock",
+        //required: true,?????
+  },
+  packageId:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "packageType",
+        required :  true
+}
+},{ timestamps: true }
+);
+
+module.exports = model('order', orderSchema);
+
