751fecd44aa83faee565f4098c760a57944e31d8
Prashan Samarathunge <prashansamarathunge@gmail.com>
2024-01-30 23:28:07 +0530

    "userSchema.js"

---
+++ models/userSchema.js
@@ -0,0 +1,59 @@
+
+const mongoose = require('mongoose');
+const { Schema, model} = mongoose;
+
+const userSchema = new Schema({
+
+    //Need Foreign Keys. Remove this line after reviewing foreign keys.
+    userId:{
+        type : Number,
+        required :  true,
+        unique :  true
+    },
+    firstName:{
+        type : String,
+        required :  true,
+        maxLength : 50
+    },
+    lastName:{
+        type : String,
+        maxLength : 50
+    },
+    email:{
+        type : String,
+        required : true,
+        match :  email
+    },
+    contactNumber:{
+        type : Number,
+        required :  true,
+        maxLength :  15,
+        minLength :  10
+
+    },
+    address:{
+        type : addressSchema
+    },
+    username:{
+        type : String,
+        required : true
+    },
+    password:{
+        type : String,
+        match :  password,
+        required :  true,
+        minLength :  8,
+        maxLength :  50
+
+    },
+    countryId:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "country",
+        required :  true
+   }
+
+},{ timestamps: true }
+);
+
+module.exports = model('user', userSchema);
+
