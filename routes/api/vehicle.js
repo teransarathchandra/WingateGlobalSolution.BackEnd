f636099c162522efba610e3d2b728c3e0411bd94
Dasuni <dasunimandini@gmail.com>
2024-02-01 17:44:46 +0530

    Update Vehicle Added

--- routes/api/vehicle.js
+++ routes/api/vehicle.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const { getAllVehicles, getvehicleById, createVehicle } =  require('../../controller/vehicleController');
+const { getAllVehicles, getvehicleById, createVehicle, updateVehicle } =  require('../../controller/vehicleController');
 
 router.get('/', getAllVehicles);
 router.get('/:id', getvehicleById);
 router.post('/', createVehicle);
+router.put('/:id', updateVehicle);
 
 module.exports = router;
