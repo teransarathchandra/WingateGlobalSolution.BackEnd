503a392cade49ce46dd35ccf0744d5773b1de530
Yethmi Gamage <yethmigamage@gmail.com>
2024-02-02 14:16:13 +0530

    items

---
+++ schemas/itemSchema.js
@@ -0,0 +1,13 @@
+const Joi = require('joi');
+
+const itemJoiSchema = Joi.object({
+  itemId: Joi.number().required(),
+  name: Joi.string(),
+  description: Joi.string(),
+  weight: Joi.number().required(),
+  value: Joi.number(),
+  orderId: Joi.number().required(), 
+  categoryId: Joi.number().required(),
+});
+
+module.exports = itemJoiSchema;
