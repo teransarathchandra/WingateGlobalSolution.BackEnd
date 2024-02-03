7303c857bdab850bbdce0b295dd1d2bc19951a2d
Dinusha Nadeeshan <dnshan46@gmail.com>
2024-02-03 12:53:50 +0530

    Customer & User API Configuration

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,11 +2,22 @@ const router = require('express').Router();
 
 const employeeRoutes = require('./employee');
 const categoryRoutes = require('./category');
+const userRoutes = require('./user');
+ const customerRoutes = require('./customer');
+
 
 // employee routes
 router.use('/employee', employeeRoutes);
 
 //category routes
 router.use('/category', categoryRoutes)
+
+//user routes
+router.use('/user', userRoutes);
+
+//customer routes
+ router.use('/customer', customerRoutes);
+
+
 
 module.exports = router;
