9e2f38c8d293f18a12211e77af612a1e508ccb89
Yethmi Gamage <yethmigamage@gmail.com>
2024-02-19 19:15:46 +0530

    Quotation auto Increment ID

--- schemas/quotation.schema.js
+++ schemas/quotation.schema.js
@@ -1,12 +1,12 @@
 const Joi = require('joi');
 
 const quotationJoiSchema = Joi.object({
-    quotationId: Joi.number().required(),
+    quotationId: Joi.string(),
     packagingCost: Joi.number().min(0).required(),
     routeCost: Joi.number().min(0).required(),
     unitWeightCost: Joi.number().min(0).required(), 
     surcharge: Joi.number().min(0).required(),
-    orderId: Joi.string().required(), // Assuming this is a string representation of ObjectId
+    orderId: Joi.string().required(), 
 });
 
 module.exports = quotationJoiSchema;
