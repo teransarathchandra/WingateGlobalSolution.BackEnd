154f4aa74426f0ee48a6982cd0225ceffa40a675
Yethmi Gamage <yethmigamage@gmail.com>
2024-02-19 21:37:34 +0530

    Driver auto increment ID

--- schemas/driver.schema.js
+++ schemas/driver.schema.js
@@ -1,7 +1,8 @@
 const Joi = require('joi');
 
 const driverJoiSchema = Joi.object({
-    driverId: Joi.string().required(),
+    driverId: Joi.string(),
+    employeeId: Joi.string().required(),
     warehouseId: Joi.string().required() 
 });
 
