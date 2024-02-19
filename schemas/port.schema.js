857a1b0fb10f5b3e004c3f95eb6cca596588eb3b
Yethmi Gamage <yethmigamage@gmail.com>
2024-02-19 19:10:12 +0530

    Port aut Increment ID

--- schemas/port.schema.js
+++ schemas/port.schema.js
@@ -2,7 +2,7 @@ const Joi = require('joi');
 
 const portJoiSchema = Joi.object({
 
-    portId: Joi.number().required(),
+    portId: Joi.string(),
     portCode: Joi.string().required().min(2).max(5),
     name: Joi.string().required().min(10).max(255),
     type: Joi.string().required().min(5).max(20),
