ff167c7d8200f62f5d12dbfe318031a09685f957
Yethmi Gamage <yethmigamage@gmail.com>
2024-02-20 11:40:46 +0530

    UserName min length changed 5 to 2

--- schemas/user.schema.js
+++ schemas/user.schema.js
@@ -3,8 +3,8 @@ const Joi = require('joi');
 const { regExp } = require('../constants');
 
 const nameSchema = Joi.object({
-    firstName: Joi.string().required().min(5).max(255).regex(regExp.name),
-    lastName: Joi.string().required().min(5).max(255).regex(regExp.name)
+    firstName: Joi.string().required().min(2).max(255).regex(regExp.name),
+    lastName: Joi.string().required().min(2).max(255).regex(regExp.name)
 });
 
 const addressSchema = Joi.object({
