1c64881bcfb56970552c716f4f5b35329cd3d363
dnshan <dnshan46@gmail.com>
2024-03-31 16:03:57 +0530

    #330 User API Minor Changes

--- routes/api/user.route.js
+++ routes/api/user.route.js
@@ -6,7 +6,7 @@ const { userController } = require('../../controllers');
 router.get('/', userController.getAllUsers);
 router.get('/:id', userController.getUserById);
 router.post('/', userController.createUser);
-router.put('/:id', userController.updateUser);
+router.patch('/:id', userController.updateUser);
 router.delete('/:id', userController.deleteUser);
 router.post('/login', userController.loginUser);
 router.post('/auth/google', userController.googleSignIn);
