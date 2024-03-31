37ef2936be15c67b0e0d91007cd5fb2fb7e8cb82
Yethmi Gamage <yethmigamage@gmail.com>
2024-03-31 12:37:10 +0530

    #282 API configuration and aggregation folder

--- schemas/country.schema.js
+++ schemas/country.schema.js
@@ -3,9 +3,7 @@ const Joi = require('joi');
 const countryJoiSchema = Joi.object({
   countryCode: Joi.string().required().min(2).max(5),
   name: Joi.string().required().min(10).max(255),
-  mobileCode: Joi.string().required().min(2).max(10),
   currency: Joi.string().required().min(1).max(5),
-  exchangeRate: Joi.number().required(),
 })
 
 module.exports = countryJoiSchema;
