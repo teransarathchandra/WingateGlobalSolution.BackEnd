c3381e0a637169fc445900d78f48ad15ae523b1c
Yethmi Gamage <yethmigamage@gmail.com>
2024-04-03 20:42:43 +0530

    #282 Restricted Order Type Create Done

--- aggregates/restrictedOrder.aggregate.js
+++ aggregates/restrictedOrder.aggregate.js
@@ -37,6 +37,57 @@ const aggTypeOne = [
     }
 ];
 
-const aggTypeTwo = [];
+const restrictedOrderTypes = [[
+        {
+          '$lookup': {
+            'from': 'countries', 
+            'localField': 'sendingCountryId', 
+            'foreignField': '_id', 
+            'as': 'sendingCountryName'
+          }
+        }, {
+          '$lookup': {
+            'from': 'countries', 
+            'localField': 'receivingCountryId', 
+            'foreignField': '_id', 
+            'as': 'receivingCountryName'
+          }
+        }, {
+          '$lookup': {
+            'from': 'categories', 
+            'localField': 'categoryId', 
+            'foreignField': '_id', 
+            'as': 'categoryName'
+          }
+        }, {
+          '$unwind': {
+            'path': '$sendingCountryName'
+          }
+        }, {
+          '$unwind': {
+            'path': '$receivingCountryName'
+          }
+        }, {
+          '$unwind': {
+            'path': '$categoryName'
+          }
+        }, {
+          '$project': {
+            '_id': 1, 
+            'restrictedOrderId': 1, 
+            'maxQuantity': 1, 
+            'exportLicense': 1, 
+            'importPermit': 1, 
+            'safetyDataSheets': 1, 
+            'phytosanitaryCertificate': 1, 
+            'dangerousGoodsDeclaration': 1, 
+            'categoryId': '$categoryName.name', 
+            'sendingCountryId': '$sendingCountryName.countryCode', 
+            'receivingCountryId': '$receivingCountryName.countryCode'
+          }
+        }
+      ]
 
-module.exports = { aggTypeOne, aggTypeTwo };
+];
+
+module.exports = { aggTypeOne, restrictedOrderTypes };
--- schemas/country.schema.js
+++ schemas/country.schema.js
@@ -2,7 +2,7 @@ const Joi = require('joi');
 
 const countryJoiSchema = Joi.object({
   countryCode: Joi.string().required().min(2).max(5),
-  name: Joi.string().required().min(10).max(255),
+  name: Joi.string().required().min(1).max(255),
   currency: Joi.string().required().min(1).max(5),
 })
 
