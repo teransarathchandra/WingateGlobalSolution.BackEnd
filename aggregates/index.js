08088deadb024f9ce5f57b2ee59ff7fe01e43235
Dasuni Premasinghe <dasunimandini@gmail.com>
2024-04-20 13:09:22 +0530

    #422 Order Tracking

--- aggregates/index.js
+++ aggregates/index.js
@@ -1,5 +1,6 @@
 const restrictedOrderAgg = require('./restrictedOrder.aggregate');
 const transportAgg = require('./transport.aggregate');
 const orderAgg = require('./order.aggregate');
+const crmAgg = require('./crm.aggregate');
 
-module.exports = { restrictedOrderAgg, transportAgg, orderAgg };
+module.exports = { restrictedOrderAgg, transportAgg, orderAgg, crmAgg };
--- routes/api/order.route.js
+++ routes/api/order.route.js
@@ -7,7 +7,7 @@ const { isAuthorized } = require('../../middlewares');
 router.get('/', isAuthorized, orderController.getAllOrder);
 router.get('/orderTransport', isAuthorized, orderController.getAllOrderTransport);
 router.get('/orderInfo', isAuthorized, orderController.getAllOrderInfo);
-router.get('/byObjectId/:id', isAuthorized, orderController.getOrderById); // Changed to '/byObjectId/:id'
+router.get('/:id', isAuthorized, orderController.getOrderById); // Changed to '/byObjectId/:id'
 router.get('/byOrderId/:orderId', isAuthorized, orderController.getOrderByOrderId);
 router.post('/', isAuthorized, orderController.createOrder);
 router.put('/:id', isAuthorized, orderController.updateOrder);
