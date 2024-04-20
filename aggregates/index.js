ed70f66fdcf15fcd9de53d404045bd8421882a13
dnshan <dnshan46@gmail.com>
2024-04-20 11:26:46 +0530

    #422 Minor Changes

--- aggregates/index.js
+++ aggregates/index.js
@@ -1,4 +1,6 @@
 const restrictedOrderAgg = require('./restrictedOrder.aggregate');
 const transportAgg = require('./transport.aggregate');
+const orderAgg = require('./order.aggregate')
+const userOrdersAgg = require('./userOrders.aggregate')
 
 module.exports = { restrictedOrderAgg, transportAgg, orderAgg, userOrdersAgg };
